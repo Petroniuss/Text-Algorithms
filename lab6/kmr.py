8940bbe6e827187d8b8a83916ddb232b04c48880
Petroniuss <wojtyczekPatryk@gmail.com>
2020-06-06 16:06:11 +0200

    finished lab6

--- lab6/kmr.py
+++ lab6/kmr.py
@@ -1,68 +1,6 @@
 import math
 
-# Slighly improved implementation of dr Pohl
-
-
-# def pohl_sort_rename(sequence):
-#     last_entry = None
-#     index = 0
-#     position_to_index = [None] * len(sequence)
-#     first_entry = {}
-
-#     for entry in sorted([(e, i) for i, e in enumerate(sequence)]):
-#         if (last_entry and last_entry[0] != entry[0]):
-#             index += 1
-#             first_entry[index] = entry[1]
-#         elif last_entry is None:
-#             first_entry[0] = entry[1]
-
-#         position_to_index[entry[1]] = index
-#         last_entry = entry
-
-#     return (position_to_index, first_entry)
-
-
-# def pohl_kmr(text, pad_with=chr(0x10ffff)):
-#     original_len = len(text)
-#     factor = math.floor(math.log2(len(text)))
-#     max_len = 2 ** factor
-#     padding_len = 2 ** (factor) - 1
-#     text += pad_with * padding_len
-
-#     position_to_index, first_entry = pohl_sort_rename(list(text))
-#     names = {1: position_to_index}
-#     entries = {1: first_entry}
-
-#     for i in range(1, factor):
-#         power = 2 ** (i - 1)
-#         new_seq = []
-#         for j in range(len(names[power]) - power):
-#             new_seq.append((names[power][j], names[power][j + power]))
-
-#         position_to_index, first_entry = pohl_sort_rename(new_seq)
-#         names[power * 2] = position_to_index
-#         entries[power * 2] = first_entry
-
-#     return (names, entries)
-
-
-# def pohl_search_pattern(pattern, text, break_with=chr(0x10ffff)):
-#     m = len(pattern)
-
-#     new_text = pattern + break_with + text
-#     names, entries = pohl_kmr(new_text)
-
-#     t = 2 ** math.floor(math.log2(m))
-
-#     name1, name2 = names[t][0], names[t][m - t]
-#     for i in range(m + 1, len(new_text)):
-#         cand_name1, cand_name2 = names[t][i], names[t][i + m - t]
-
-#         if name1 == cand_name1 and name2 == cand_name2:
-#             yield (i - m - 1)
-
-
-# -----------------------------------------------------------------------
+SPECIAL = chr(0x10ffff)
 
 # Tweaked implementation!
 
@@ -72,8 +10,9 @@ import math
 # 3. Implement searching for pattern using basic approach. -> DBF(pat&text)
 # 4. Implement searching using pos table and binary search yielding O(n + m*log(n))
 #           We could reach O(mlog(n)) time complexity if we were to increase space complexity..
-SPECIAL = chr(0x10ffff)
 
+
+# Implementation of sort_rename and kmr is basically tweaked implementation from the lecture.
 
 def sort_rename(sequence, seq_of_chars=False):
     """
@@ -165,8 +104,10 @@ def efficient_search(pattern, text, dbf):
 
     names, pos = dbf
     m, n = len(pattern), len(text)
-    t = 2 ** math.floor(math.log2(m))
+    if m > n or m <= 0:
+        raise Exception('Invalid pattern!')
 
+    t = 2 ** math.floor(math.log2(m))
     name = bin_search_name(pattern[:t], text, dbf)
 
     # if found
@@ -218,11 +159,11 @@ def show_dbf(dbf, text):
         Prints name and pos tables in the same manner as shown on the lecture.
     """
     names, pos = dbf
-    print('names')
+    print('\nnames')
     for k, v in names.items():
-        print(k, [e + 1 for e in v[:len(text)]])
+        print(k, v[:len(text)])
 
-    print('pos')
+    print('\npos')
     for k, v in pos.items():
         print(k, [v[e] + 1 for e in range(len(v) - 1)])
 
